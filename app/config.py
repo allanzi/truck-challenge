7734a011de899184b673e99fd1c2ff92a6af65b9
Allan Santos <allanzi.santos@gmail.com>
2020-04-04 20:34:56 -0300

    Fix CORS

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
